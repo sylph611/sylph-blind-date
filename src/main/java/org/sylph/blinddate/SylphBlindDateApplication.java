070374105b812f83282b32ad11b1095898c19519
sylph611 <ccg0611a@gmail.com>
2024-10-28 06:21:39 +0900

    JPA Auditing enabled

--- src/main/java/org/sylph/blinddate/SylphBlindDateApplication.java
+++ src/main/java/org/sylph/blinddate/SylphBlindDateApplication.java
@@ -2,8 +2,10 @@ package org.sylph.blinddate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SylphBlindDateApplication {
 
     public static void main(String[] args) {
