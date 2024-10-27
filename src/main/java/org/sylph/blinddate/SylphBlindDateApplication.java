178fd5e171a563376a96ffa417860496d24ca529
sylph611 <ccg0611a@gmail.com>
2024-10-27 10:08:21 +0900

    init

---
+++ src/main/java/org/sylph/blinddate/SylphBlindDateApplication.java
@@ -0,0 +1,13 @@
+package org.sylph.blinddate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SylphBlindDateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SylphBlindDateApplication.class, args);
+    }
+
+}
