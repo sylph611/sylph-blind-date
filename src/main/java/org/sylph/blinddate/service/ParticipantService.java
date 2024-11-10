3475f70f265c8886e92da22c097cff12fd46b22d
sylph611 <ccg0611a@gmail.com>
2024-11-10 15:17:37 +0900

    주석추가

--- src/main/java/org/sylph/blinddate/service/ParticipantService.java
+++ src/main/java/org/sylph/blinddate/service/ParticipantService.java
@@ -16,21 +16,25 @@ public class ParticipantService {
 
     private final ParticipantRepository participantRepository;
 
+    // 참여자 리스트 조회
     public Page<ParticipantDto> getParticipants(Pageable pageable) {
         return participantRepository.findAll(pageable).map(ParticipantMapper::toDto);
     }
 
+    // 참여자 상세 조회
     public ParticipantDto getParticipant(Long id) {
         Participant participant = participantRepository.findById(id).orElseThrow(() -> new RuntimeException("Participant not found: " + id));
         return ParticipantMapper.toDto(participant);
     }
 
+    // 참여자 저장
     public void saveParticipant(ParticipantDto participantDto) {
         Participant participant = ParticipantMapper.toEntity(participantDto);
         if(ObjectUtils.isEmpty(participant)) throw new RuntimeException("Participant is empty");
         participantRepository.save(participant);
     }
 
+    // 참여자 수정
     public void updateParticipant(Long id, ParticipantDto dto) {
         participantRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Participant not found: " + id));
@@ -39,6 +43,7 @@ public class ParticipantService {
         participantRepository.save(participant);
     }
 
+    // 참여자 삭제
     public void deleteParticipant(Long id) {
         participantRepository.deleteById(id);
     }
